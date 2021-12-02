bee1210736d5ffb66ca303f6ed42e269ed03a86d
Felipe Cardozo <fraifelipe@gmail.com>
2021-12-02 03:08:47 +0000

    open tezos rpc ports

--- node-configurator/tezos-node-configurator.sh
+++ node-configurator/tezos-node-configurator.sh
@@ -18,13 +18,9 @@ mkdir -p ${node_dir}/data
 cat << EOF > ${node_dir}/data/config.json
 { "data-dir": "/var/run/tezos/node/data",
   "network": "$TEZOS_NETWORK",
-  "rpc": { "listen-addrs": [ ":8732", "0.0.0.0:8732" ],
-      "cors-origin":
-        [ "*" ],
-      "cors-headers": [ "content-type" ],
-      "acl":
-        [ { "address": ":8732", "blacklist": [] } ]
-    },
+  "rpc":
+    { "listen-addrs": [ ":8732" ],
+      "acl": [ { "address": ":8732", "blacklist": [] } ] },
   "p2p":
     { "limits":
         { "connection-timeout": 10, "min-connections": 25,
